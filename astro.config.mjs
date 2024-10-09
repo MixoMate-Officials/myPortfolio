7991889dcb1a9d5d7ce4f20c84c5e56d4187a238
rockvns <chandrabhanvns@gmail.com>
2024-10-10 05:18:57 +0530

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
-
+import github from '@astrojs/github';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({  site: 'https://rockvns.github.io',
+  base: 'my-repo',});
